16b968b42b293c0042dd01c85203841ca99cd901
perarivaalan4 <perarivaalan04@gmail.com>
2025-06-10 23:56:36 +0530

    first

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    
+    for(int i=0;i<5;i++)
+    {
+        System.out.println("hello world");
+    }
+}
+}
